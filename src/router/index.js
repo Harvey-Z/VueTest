5be8a65e50643e5152ec89b98583c92727596f96
zhanghongwei <670831665@qq.com>
2019-12-16 17:32:11 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
+const Home = (resolve) => {
+  import('@/components/Home').then(module=>{
+    resolve(module)
+  })
+}
+
+const router = new Router({
+  mode:'history',
+  routes:[
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
+
+export default router
